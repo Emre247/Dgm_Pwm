eb60c930ef64a0af3478bfd6ef9a6631808b7f01
Emre247 <emre2x@msn.com>
2024-11-03 20:17:05 +0300

    try catch den çıkıldı

--- Dgm_Pwm.py
+++ Dgm_Pwm.py
@@ -4,12 +4,7 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 cikisPin=38
 GPIO.setup(cikisPin,GPIO.OUT)
-try:
 #60 herz üzerinden işlem yapıyorum. İnsan gözü bu değerden sonra
 #ki tüm değerleri ayırt edemiyor ya da çok yakın hissediyor.
-        benimDGM=GPIO.PWM(cikisPin,60)
-        benimDGM.start(30)
-except KeyboardInterrupt:
-    print("Program başarılı bir şekilde kapatıldı..")
-    GPIO.cleanup()
-
+benimDGM=GPIO.PWM(cikisPin,60)
+benimDGM.start(30)
