60caf5e286e69b867dcfa415e9584618508f2877
Emre247 <emre2x@msn.com>
2024-11-03 20:07:28 +0300

    while döngüsü silindi

--- Dgm_Pwm.py
+++ Dgm_Pwm.py
@@ -5,7 +5,6 @@ GPIO.setmode(GPIO.BOARD)
 cikisPin=38
 GPIO.setup(cikisPin,GPIO.OUT)
 try:
-    while True:
 #60 herz üzerinden işlem yapıyorum. İnsan gözü bu değerden sonra
 #ki tüm değerleri ayırt edemiyor ya da çok yakın hissediyor.
         benimDGM=GPIO.PWM(cikisPin,60)
