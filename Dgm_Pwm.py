b151f2e31c30e676cda45ad3cd5ea4e7153b53b2
Emre247 <emre2x@msn.com>
2024-11-03 22:50:24 +0300

    kod dene ve hata bloğunun içeirisineauyku modülüyle birlikte alındı

--- Dgm_Pwm.py
+++ Dgm_Pwm.py
@@ -1,7 +1,16 @@
 import RPi.GPIO as GPIO
+from time import sleep
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38,GPIO.OUT)
-GPIO.output(38,True)
-GPIO.output(38,False)
-benimDGM=GPIO.PWM(38,100)
-benimDGM.start(5)
+# PWM frekansını 100 Hz olarak ayarlıyoruz
+benimDGM = GPIO.PWM(38, 100)
+benimDGM.start(5)  # %5 "duty cycle" ile düşük parlaklıkta başlat
+try:
+    while True:
+        sleep(1)  # Sonsuz döngüde bekletiyoruz
+except KeyboardInterrupt:
+    pass  # Çıkmak için Ctrl+C tuşlayabilirsiniz
+    print("Herşey yolunda..")
+finally:
+    benimDGM.stop()
+    GPIO.cleanup()
