f008732484aa7c5199a591a3f0a5dc65ed6ae903
Emre247 <emre2x@msn.com>
2024-11-03 22:51:20 +0300

    kod dene ve hata bloğunun içeirisineauyku modülüyle birlikte alındı

--- Dgm_Pwm.py
+++ Dgm_Pwm.py
@@ -4,7 +4,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38,GPIO.OUT)
 # PWM frekansını 100 Hz olarak ayarlıyoruz
 benimDGM = GPIO.PWM(38, 100)
-benimDGM.start(5)  # %5 "duty cycle" ile düşük parlaklıkta başlat
+benimDGM.start(50)  # %5 "duty cycle" ile düşük parlaklıkta başlat
 try:
     while True:
         sleep(1)  # Sonsuz döngüde bekletiyoruz
