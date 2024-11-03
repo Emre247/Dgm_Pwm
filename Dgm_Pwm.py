5ba601ec85959472a757b7afa9840d3b529ea7b4
Emre247 <emre2x@msn.com>
2024-11-03 20:24:54 +0300

    akım düzenli hale getirildi

--- Dgm_Pwm.py
+++ Dgm_Pwm.py
@@ -4,7 +4,8 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 cikisPin=38
 GPIO.setup(cikisPin,GPIO.OUT)
+GPIO.output(cikisPin,True)
 #60 herz üzerinden işlem yapıyorum. İnsan gözü bu değerden sonra
 #ki tüm değerleri ayırt edemiyor ya da çok yakın hissediyor.
 benimDGM=GPIO.PWM(cikisPin,60)
-benimDGM.start(100)
+benimDGM.start(30)
